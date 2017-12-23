4517225b5d8be1a0eaf544826704786391214093
LaloSanchez <eduardo_sz5k4@hotmail.com>
2017-12-23 11:23:25 -0600

    este

--- TalentoUAQ/Views/DescripcionCurriulum.xaml.cs
+++ TalentoUAQ/Views/DescripcionCurriulum.xaml.cs
@@ -6,25 +6,9 @@ namespace TalentoUAQ
 {
     public partial class DescripcionCurriculum : ContentPage
     {
-        public Item Item { get; set; }
-
         public DescripcionCurriculum()
         {
-            //InitializeComponent();
-
-            Item = new Item
-            {
-                Text = "Item name",
-                Description = "This is an item description."
-            };
-
-            BindingContext = this;
-        }
-
-        async void Save_Clicked(object sender, EventArgs e)
-        {
-            MessagingCenter.Send(this, "AddItem", Item);
-            await Navigation.PopToRootAsync();
+            InitializeComponent();
         }
     }
 }

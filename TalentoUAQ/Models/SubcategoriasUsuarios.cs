a39852e63fbfd01d442d4ff02ba112329850dd71
antonioantunez7 <scorpion_malboro@hotmail.com>
2017-12-28 14:24:45 -0600

    a

---
+++ TalentoUAQ/Models/SubcategoriasUsuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TalentoUAQ.Models
+{
+    public class SubcategoriasUsuarios
+    {
+        public SubcategoriasUsuarios()
+        {
+        }
+
+        public int idSubcategoriaUsuario{
+            get;
+            set;
+        }
+
+        public string desSubcategoria{
+            get;
+            set;
+        }
+    }
+}

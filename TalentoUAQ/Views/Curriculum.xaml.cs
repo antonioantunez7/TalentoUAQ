690c02d7badabe3ef298717fef1880498c4a82db
LaloSanchez <eduardo_sz5k4@hotmail.com>
2018-01-10 00:35:23 -0600

    tamaño dinamico de listViews

--- TalentoUAQ/Views/Curriculum.xaml.cs
+++ TalentoUAQ/Views/Curriculum.xaml.cs
@@ -49,6 +49,9 @@ namespace TalentoUAQ.Views
             subcategoriasUsuarios.Add(new SubcategoriasUsuarios { idSubcategoriaUsuario = 2, desSubcategoria = "Desarrollador PHP" });
             subcategoriasUsuarios.Add(new SubcategoriasUsuarios { idSubcategoriaUsuario = 3, desSubcategoria = "Desarrollador .NET" });
             subcategoriasUsuarios.Add(new SubcategoriasUsuarios { idSubcategoriaUsuario = 4, desSubcategoria = "Desarrollador Java" });
+            int totalSubcategorias = subcategoriasUsuarios.Count;
+            Double total = 35 * totalSubcategorias;
+            ListaSubcategoriasUsuarios.HeightRequest = total;
             ListaSubcategoriasUsuarios.ItemsSource = subcategoriasUsuarios;
         }
         void cargarEstudios()
@@ -56,6 +59,8 @@ namespace TalentoUAQ.Views
             estudios = new ObservableCollection<Estudios>();
             estudios.Add(new Estudios { idEstudio = 1, escuela = "Universiad Tecnológica del Valle de Toluca", carrera = "Tecnologias de la Información y Comunicación", fechaInicio = "01/10/2015", fechaFin = "01/10/2018" });
             estudios.Add(new Estudios { idEstudio = 2, escuela = "Centro de Estudios Tecnológicos industrial y de servicios #23", carrera = "Informatica", fechaInicio = "01/10/2012", fechaFin = "01/10/2015" });
+            Double totalEstudios = 80 * estudios.Count;
+            ListaEstudios.HeightRequest = totalEstudios;
             ListaEstudios.ItemsSource = estudios;
         }
         void cargarExperiencia()
@@ -64,6 +69,8 @@ namespace TalentoUAQ.Views
             experiencias.Add(new Experiencias { idExperiencia = 1, empresa = "Andrades Company", puesto = "Desarrollador Movil", fechaInicio = "01/10/2015", fechaFin = "01/10/2018" });
             experiencias.Add(new Experiencias { idExperiencia = 2, empresa = "Poder Judicial del Estado de México", puesto = "Desarrollador Web", fechaInicio = "01/10/2015", fechaFin = "01/10/2018" });
             experiencias.Add(new Experiencias { idExperiencia = 3, empresa = "Secrtaría de la Contraloria del Gobierno del estado de México", puesto = "Desarrollador", fechaInicio = "01/10/2015", fechaFin = "01/10/2018" });
+            Double totalExperiencia = 80 * experiencias.Count;
+            ListaExperiencia.HeightRequest = totalExperiencia;
             ListaExperiencia.ItemsSource = experiencias;
         }
         void cargarIdiomas()
@@ -71,6 +78,8 @@ namespace TalentoUAQ.Views
             idiomasModel = new ObservableCollection<IdiomasModel>();
             idiomasModel.Add(new IdiomasModel { idIdioma = 1, idioma = "Español", porcentaje = 100 });
             idiomasModel.Add(new IdiomasModel { idIdioma = 1, idioma = "ingles", porcentaje = 50 });
+            Double totalIdiomas = 35 * idiomasModel.Count;
+            ListaIdiomas.HeightRequest = totalIdiomas;
             ListaIdiomas.ItemsSource = idiomasModel;
         }
         async void eliminarEspecialidad_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
